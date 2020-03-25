2cbaf50b9a20eb5a49c370928221ff25eefcddc9
JudgeFudge19 <dremukare@gmail.com>
2020-03-25 14:36:40 +0300

    Solved li append issue. Added filter function, added functionality to clear tasks button, persisted tasks to local storage

--- app.js
+++ app.js
@@ -11,8 +11,48 @@ loadEventListeners();
 
 // Defining function to load all event listeners
 function loadEventListeners() {
+  // DOM Load event
+  document.addEventListener('DOMContentLoaded', getTasks);
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click',removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click',clearTasks);
+  // Filter task event
+  filter.addEventListener('keyup',filterTasks);
+}
+
+// Get tasks from Local Storage
+function getTasks() {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = []
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(
+    function(task){
+      // Create li element 
+      const li = document.createElement('li');
+      // Add class to created element
+      li.className = 'collection-item';
+      // Create text node and append to the li
+      li.appendChild(document.createTextNode(task));
+      // Create new link element
+      const link = document.createElement('a');
+      // Add class
+      link.className = 'delete-item secondary-content';
+      // Adding icon through innerHTML
+      link.innerHTML = '<i class="fa fa-remove"></i>';
+      // Append the link to the li
+      li.appendChild(link);
+
+      // Append li to ul
+      taskList.appendChild(li);
+    }
+  );
 }
 
 // Add task function
@@ -21,15 +61,12 @@ function addTask(e) {
     alert('Add a task first!');
   }
 
-  let value = taskInput.value;
-
   // Create li element 
-  document.createElement('li');
+  const li = document.createElement('li');
   // Add class to created element
   li.className = 'collection-item';
   // Create text node and append to the li
-  let liTextNode = document.createTextNode(value); 
-  li.appendChild(liTextNode);
+  li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
   const link = document.createElement('a');
   // Add class
@@ -42,8 +79,93 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li);
 
+  // Store in local storage
+  storeTaskInLocalStorage(taskInput.value);
+
   // Clear input field
-  value = '';
+  taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Saving tasks to Local Storage
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = []
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove task function 
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+
+      // Remove from local storage
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+    }
+  }
+}
+
+// Remove from local storage function
+function removeTaskFromLocalStorage(taskItem){
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = []
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(
+    function(task, index){
+      if(taskItem.textContent === task){
+        tasks.splice(index, 1);
+      }
+    }
+  );
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Clear task function
+function clearTasks(e) {
+  if(confirm('Are you sure?')){
+    while(taskList.firstChild){
+      taskList.removeChild(taskList.firstChild);
+    }// while there is a firstChild, remove the firstChild if there still is one
+
+    // Clear tasks from local storage
+    clearTasksFromLocalStorage();
+  }
+
+  // Alternative is to use taskList.innerHTML = ''; 
+  // Literally setting the HTML within taskList which is a ul to blank
+}
+
+// Clear tasks from local storage function
+function clearTasksFromLocalStorage(){
+  localStorage.clear(); // clear everything from local storage
+}
+
+// Filter tasks function
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection.item').forEach(
+    function(task){
+     const item = task.firstChild.textContent;
+     if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+     } else{  
+      task.style.display = 'none';
+     }
+    }
+  );
 }
